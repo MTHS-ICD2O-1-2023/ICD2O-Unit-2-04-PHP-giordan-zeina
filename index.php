1571c7b0d4ea3b97aac67a62f905a5002ce7beef
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-26 18:22:03 +0000

    button

--- index.php
+++ index.php
@@ -70,9 +70,9 @@
           <br />
           <!-- Accent-colored raised button with ripple -->
           <button
-            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            onclick="calculateAreaOfTriangle()">
-            Calculate
+            <!-- Accent-colored raised button with ripple -->
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+              Calculate
           </button>
         </form>
         <br />
